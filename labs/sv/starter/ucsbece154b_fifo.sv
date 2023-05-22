0167910c7de2926cd5f515f891514b67877e080c
Parker Murray <parker@pmurray.io>
2023-05-22 14:31:30 -0700

    Initial attempt at FIFO implementation

--- labs/sv/starter/ucsbece154b_fifo.sv
+++ labs/sv/starter/ucsbece154b_fifo.sv
@@ -20,6 +20,25 @@ module ucsbece154b_fifo #(
     output  logic                   valid_o
 );
 
-// TODO
+logic [DATA_WIDTH - 1:0] RAM [0: NR_ENTRIES - 1];
+integer head;
+integer tail;
+integer numEntries;
+
+assign full_o = numEntries == NR_ENTRIES ? 1 : 0;
+assign valid_o = numEntries == 0 ? 0 : 1;
+
+always_ff @ (posedge clk_i) begin
+    if (pop_i && valid_o) begin
+        data_o <= RAM[head];
+        head <= (head + 1) % NR_ENTRIES;
+        numEntries--;
+    end
+    if (push_i && !full_o) begin
+        RAM[tail] <= data_i;
+        tail <= (tail + 1) % NR_ENTRIES;
+        numEntries++;
+    end
+end
 
 endmodule
