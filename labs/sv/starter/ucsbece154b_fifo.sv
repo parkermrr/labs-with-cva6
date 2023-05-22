294e654a3a65a71794b30ce20a3710b65024a339
Parker Murray <parker@pmurray.io>
2023-05-22 15:55:37 -0700

    Rewrite in light of new understanding of always_comb vs always_ff

--- labs/sv/starter/ucsbece154b_fifo.sv
+++ labs/sv/starter/ucsbece154b_fifo.sv
@@ -7,8 +7,8 @@ module ucsbece154b_fifo #(
     parameter int unsigned DATA_WIDTH = 32,
     parameter int unsigned NR_ENTRIES = 4
 ) (
-    input   logic                   clk_i,
-    input   logic                   rst_i,
+    input   logic                   clk,
+    input   logic                   rst,
 
     output  logic [DATA_WIDTH-1:0]  data_o,
     input   logic                   pop_i,
@@ -21,23 +21,42 @@ module ucsbece154b_fifo #(
 );
 
 logic [DATA_WIDTH - 1:0] RAM [0: NR_ENTRIES - 1];
-integer head;
-integer tail;
-integer numEntries;
+logic [3:0] head_d, head_q, tail_d, tail_q;
+logic [3:0] numEntries_d, numEntries_q;
+logic write_allowed;
 
-assign full_o = numEntries == NR_ENTRIES ? 1 : 0;
-assign valid_o = numEntries == 0 ? 0 : 1;
+assign full_o = (numEntries_q == NR_ENTRIES);
+assign valid_o = (numEntries_q =! 0);
+assign data_o = valid_o ? RAM[head_q] : '0;
 
-always_ff @ (posedge clk_i) begin
+always_comb begin
+    head_d = head_q;
+    tail_d = tail_q;
+    numEntries_d = numEntries_q;
+    write_allowed = 1b'0;
+
     if (pop_i && valid_o) begin
-        data_o <= RAM[head];
-        head <= (head + 1) % NR_ENTRIES;
-        numEntries--;
+        head_d = (head_d == NR_ENTRIES - 1) ? '0 : head_d + 1;
+        numEntries_d--;
     end
     if (push_i && !full_o) begin
-        RAM[tail] <= data_i;
-        tail <= (tail + 1) % NR_ENTRIES;
-        numEntries++;
+        tail_d = (tail_d == NR_ENTRIES - 1) ? '0 : tail_d + 1;
+        write_allowed = 1b'1;
+    end
+end
+
+always_ff @ (posedge clk) begin
+    if (rst) begin
+        head_q <= '0;
+        tail_q <= '0;
+        numEntries_q <= '0;
+    end else
+        head_q <= head_d;
+        tail_q <= tail_d;
+        numEntries_q <= numEntries_d;
+        if (write_allowed) begin
+            RAM[tail_q] <= data_i;
+        end
     end
 end
 
